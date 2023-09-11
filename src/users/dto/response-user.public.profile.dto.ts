7b8be7cfd117057cbc0860879f5a694035de16de
Polina Komarova <102168218+polinakoma@users.noreply.github.com>
2023-09-11 13:24:26 +0300

    Delete src directory

--- src/users/dto/response-user.public.profile.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { UserProfileResponseDto } from './response-user.profile.dto';
-
-export class UserPublicProfileResponseDto extends OmitType(
-  UserProfileResponseDto,
-  ['email'],
-) {}
--- src/wishlists/dto/update-wishlist.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { CreateWishlistDto } from './create-wishlist.dto';
-
-export class UpdateWishlistDto extends OmitType(CreateWishlistDto, [
-  'description',
-]) {}
