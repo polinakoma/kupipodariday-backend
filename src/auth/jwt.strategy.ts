174deb4da976a42345e81c052da28f1c68309022
Polina Komarova <102168218+polinakoma@users.noreply.github.com>
2023-09-11 13:25:24 +0300

    bug fix

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private userService: UsersService,
+    private configService: ConfigService,
+  ) {
+    // console.log(configService.get<string>('JWT_SECRET'));
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: any) {
+    return this.userService.findById(payload.id);
+  }
+}
